103ffe9a22e3c16ffabd5a60ebdae123a317bd21
memecoder <memecoder17@gmail.com>
2025-05-08 23:06:01 +0700

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@
 #                          ! EDUCATIONAL PURPOSES ONLY !                       #
 ################################################################################
 # Lưu ý: đây là mã độc DCry, được phát triển để thực hiện một cuộc tấn công thực thụ vào một hệ thống máy chủ.
-# Vì vậy, Mã độc này có thể gây ra thiệt hại nghiêm trọng cho hệ thống và dữ liệu của bạn.
+# Vì vậy, mã độc này có thể gây ra thiệt hại nghiêm trọng cho hệ thống và dữ liệu của bạn.
 # Nên hãy sử dụng mã này một cách cẩn thận và chỉ được sử dụng trong các cuộc tấn công mạng giả lập nâng cao dưới sự giám sát của ít nhất một chuyên gia về an ninh mạng.
 
 from setuptools import setup
